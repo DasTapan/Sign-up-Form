75a105dc2e31daa897f7021ff2827b8b4350a8da
Tapan Das <tapands.das@gmail.com>
2022-07-24 10:31:52 +0530

    Add script file

---
+++ script.js
@@ -0,0 +1,27 @@
+const createAccountButton = document.querySelector('#create-btn');
+console.log(createAccountButton);
+const passwordButton = document.querySelector('#pswd');
+console.log(passwordButton);
+const confirmPasswordButton = document.querySelector('#pswd-confirm');
+console.log(confirmPasswordButton);
+
+let passwordValue = '';
+let confirmPasswordValue = '';
+
+createAccountButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('khi khi');
+    console.log(`Password: ${passwordButton.value}`);
+    console.log(`Confirm Password: ${confirmPasswordButton.value}`);
+
+    passwordValue = passwordButton.value;
+    confirmPasswordValue = confirmPasswordButton.value;
+
+    if (passwordValue === confirmPasswordValue) {
+        console.log('hue hue');
+    } else {
+        console.log('kya matlab ghar se nikal');
+        passwordButton.classList.add('mismatch');
+        confirmPasswordButton.classList.add('mismatch');
+    }
+});
